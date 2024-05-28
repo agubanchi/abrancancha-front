0e5efb52289e13fcd04a57d41a191a7587f89ab6
agustin banchi <agustinbanchi@MacBook-Air-de-agustin.local>
2024-05-28 20:06:00 -0300

    actualizado el codigo de detalereserva

--- src/components/DetalleReserva.jsx
+++ src/components/DetalleReserva.jsx
@@ -1,13 +1,111 @@
+// import { FaEdit } from "react-icons/fa";
+// import { MdDelete } from "react-icons/md";
+// import { useState } from "react";
+// import Swal from 'sweetalert2';
+// import useStore from "../userStore";
+// import { useAuth } from "../context/AuthContext";
+
+
+// export default function DetalleReserva({ reserva }) {
+//   const deleteReservation = useStore((state)=>state.deleteReservation)
+//   const getReservationById = useStore((state) => state.getReservationById); 
+//   const [showReservation, setShowReservation] = useState(true);
+//   const { user } = useAuth();
+//   const handleEliminar = () => {
+//     Swal.fire({
+//       title: '¿Estás seguro?',
+//       text: 'Esta acción eliminará la reserva',
+//       icon: 'warning',
+//       showCancelButton: true,
+//       color: "#1d1d1d",
+//       iconColor: "#1d1d1d",
+//       confirmButtonColor: "#77da7e",
+//       cancelButtonColor: '#1d1d1d',
+//       confirmButtonText: 'Sí, eliminar',
+//       cancelButtonText: 'Cancelar'
+//     }).then((result) => {
+//       if (result.isConfirmed) {
+//         deleteReservation(reserva.id);
+//         Swal.fire(
+//           'Eliminado!',
+//           'La reserva ha sido eliminada.',
+//           'success'
+//         );
+//       }
+//     });
+//   };
+
+//   const confirmarReserva = () => {
+//     Swal.fire({
+//       title: "Reserva confirmada!",
+//       text: "Su Reserva ha sido confirmada con éxito",
+//       icon: "success"
+//     });
+    
+//     setShowReservation(false);
+//   };
+
+//   return (
+//     <>
+//     {showReservation ? (
+//     <div className="bg-white rounded-xl mx-5 my-10 px-5 py-10">
+//     <div className='flex items-center justify-around  '>
+//     <div className=''>
+        
+//     <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Email: {''}
+//         <span className='text-sm  font-light text-textColor font-Onest'>{user.email}</span>
+//         </p>
+//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Teléfono: {''}
+//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{user.telefono}</span>
+//         </p>
+//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Cancha: {''}
+//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.cancha}</span>
+//         </p>
+//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Tipo de Cancha: {''}
+//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.tipo}</span>
+//         </p>
+//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Fecha: {''}
+//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.date}</span>
+//         </p>   
+//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Hora: {''}
+//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.hour}</span>
+//         </p>
+//     </div>
+
+// <div>
+// <img  className="w-full " src="/logo_black.svg" alt="Abrancancha" />
+// </div>
+// </div>
+
+  
+// <div class=" flex justify-between py-2 gap-3  mt-4">
+// <button type="button" className=" flex gap-2 items-center py-2 px-6 bg-indigo-500 hover:bg-indigo-800 text-white font-bold uppercase rounded-lg text-sm" onClick={()=>getReservationById(reserva.id)}><FaEdit/> Editar Reserva</button>
+// <button type="button" className=" flex gap-2 items-center py-2 px-6 bg-red-500 hover:bg-red-800 text-white font-bold uppercase rounded-lg text-sm" onClick={()=>handleEliminar(reserva.id)}><MdDelete /> Eliminar Reserva</button>
+// </div>
+
+// <div className="w-full text-center py-6">
+// <button type="button" className=" w-full   text-center py-3 px-6 bg-acentColor text-textColor hover:bg-textColor hover:text-white font-bold uppercase rounded-lg text-sm" onClick={confirmarReserva}> Confirmar Reserva</button>
+// </div>
+
+// </div>
+//   ) : <button type="button" className="w-full text-center py-3 mb-3 px-6 bg-acentColor text-white hover:bg-white hover:text-textColor font-bold uppercase rounded-lg text-sm" onClick={() => setShowReservation(true)}>Ver mis Reserva del {user.date}</button>
+// }
+//   </>
+   
+//   );
+// }
+
+
+
+//este codigo puede eliminar la reserva por usuario, pero no me funciona la edicion:
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { useState } from "react";
 import Swal from 'sweetalert2';
 import useStore from "../userStore";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from '../context/AuthContext';
+export default function DetalleReserva({ reserva, handleDeleteReservation }) {
 
-
-export default function DetalleReserva({ reserva }) {
-  const deleteReservation = useStore((state)=>state.deleteReservation)
   const getReservationById = useStore((state) => state.getReservationById); 
   const [showReservation, setShowReservation] = useState(true);
   const { user } = useAuth();
@@ -17,15 +115,13 @@ export default function DetalleReserva({ reserva }) {
       text: 'Esta acción eliminará la reserva',
       icon: 'warning',
       showCancelButton: true,
-      color: "#1d1d1d",
-      iconColor: "#1d1d1d",
-      confirmButtonColor: "#77da7e",
-      cancelButtonColor: '#1d1d1d',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
       confirmButtonText: 'Sí, eliminar',
       cancelButtonText: 'Cancelar'
     }).then((result) => {
       if (result.isConfirmed) {
-        deleteReservation(reserva.id);
+        handleDeleteReservation(reserva.id);
         Swal.fire(
           'Eliminado!',
           'La reserva ha sido eliminada.',
@@ -51,11 +147,11 @@ export default function DetalleReserva({ reserva }) {
     <div className="bg-white rounded-xl mx-5 my-10 px-5 py-10">
     <div className='flex items-center justify-around  '>
     <div className=''>
-        
-    <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Email: {''}
-        <span className='text-sm  font-light text-textColor font-Onest'>{user.email}</span>
+
+        <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Email: {''}
+        <span className='text-sm lowercase font-light text-textColor font-Onest'>{user.email}</span>
         </p>
-        <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Teléfono: {''}
+        <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Teléfno: {''}
         <span className='text-sm uppercase font-light text-textColor font-Onest'>{user.telefono}</span>
         </p>
         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Cancha: {''}
@@ -94,99 +190,6 @@ export default function DetalleReserva({ reserva }) {
    
   );
 }
-
-
-
-//este codigo puede eliminar la reserva por usuario, pero no me funciona la edicion:
-// import { FaEdit } from "react-icons/fa";
-// import { MdDelete } from "react-icons/md";
-// import { useState } from "react";
-// import Swal from 'sweetalert2';
-// import useStore from "../userStore";
-// import { useAuth } from '../context/AuthContext';
-// export default function DetalleReserva({ reserva, handleDeleteReservation }) {
-
-//   const getReservationById = useStore((state) => state.getReservationById); 
-//   const [showReservation, setShowReservation] = useState(true);
-
-//   const handleEliminar = () => {
-//     Swal.fire({
-//       title: '¿Estás seguro?',
-//       text: 'Esta acción eliminará la reserva',
-//       icon: 'warning',
-//       showCancelButton: true,
-//       confirmButtonColor: '#d33',
-//       cancelButtonColor: '#3085d6',
-//       confirmButtonText: 'Sí, eliminar',
-//       cancelButtonText: 'Cancelar'
-//     }).then((result) => {
-//       if (result.isConfirmed) {
-//         handleDeleteReservation(reserva.id);
-//         Swal.fire(
-//           'Eliminado!',
-//           'La reserva ha sido eliminada.',
-//           'success'
-//         );
-//       }
-//     });
-//   };
-
-//   const confirmarReserva = () => {
-//     Swal.fire({
-//       title: "Reserva confirmada!",
-//       text: "Su Reserva ha sido confirmada con éxito",
-//       icon: "success"
-//     });
-    
-//     setShowReservation(false);
-//   };
-
-//   return (
-//     <>
-//     {showReservation ? (
-//     <div className="bg-white rounded-xl mx-5 my-10 px-5 py-10">
-//     <div className='flex items-center justify-around  '>
-//     <div className=''>
-
-//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Teléfono: {''}
-//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.telefono}</span>
-//         </p>
-//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Cancha: {''}
-//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.cancha}</span>
-//         </p>
-//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Tipo de Cancha: {''}
-//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.tipo}</span>
-//         </p>
-//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Fecha: {''}
-//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.date}</span>
-//         </p>   
-//         <p className='text-sm uppercase font-bold mb-3 text-textColor items-center flex gap-2'> Hora: {''}
-//         <span className='text-sm uppercase font-light text-textColor font-Onest'>{reserva.hour}</span>
-//         </p>
-//     </div>
-
-// <div>
-// <img  className="w-full " src="/logo_black.svg" alt="Abrancancha" />
-// </div>
-// </div>
-
-  
-// <div class=" flex justify-between py-2 gap-3  mt-4">
-// <button type="button" className=" flex gap-2 items-center py-2 px-6 bg-indigo-500 hover:bg-indigo-800 text-white font-bold uppercase rounded-lg text-sm" onClick={()=>getReservationById(reserva.id)}><FaEdit/> Editar Reserva</button>
-// <button type="button" className=" flex gap-2 items-center py-2 px-6 bg-red-500 hover:bg-red-800 text-white font-bold uppercase rounded-lg text-sm" onClick={()=>handleEliminar(reserva.id)}><MdDelete /> Eliminar Reserva</button>
-// </div>
-
-// <div className="w-full text-center py-6">
-// <button type="button" className=" w-full   text-center py-3 px-6 bg-acentColor text-textColor hover:bg-textColor hover:text-white font-bold uppercase rounded-lg text-sm" onClick={confirmarReserva}> Confirmar Reserva</button>
-// </div>
-
-// </div>
-//   ) : <button type="button" className="w-full text-center py-3 mb-3 px-6 bg-acentColor text-white hover:bg-white hover:text-textColor font-bold uppercase rounded-lg text-sm" onClick={() => setShowReservation(true)}>Ver mis Reserva del {user.date}</button>
-// }
-//   </>
-   
-//   );
-// }
 
 
 
--- src/components/ListadoReservas.jsx
+++ src/components/ListadoReservas.jsx
@@ -1,81 +1,21 @@
 // ListadoUsuarios.js
-import React from "react";
-import useStore from "../userStore";
-import DetalleReserva from "./DetalleReserva";
-
-export default function ListadoReservas() {
-  const reservationsList = useStore((state) => state.reservations);
-
-  return (
-    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll scrollbar scrollbar-thumb-acentColor scrollbar-thumb-radius">
-      {reservationsList.length ? (
-        <>
-          <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reserva</h2>
-          <p className="text-lg mt-5 text-center mb-10 text-white">Verifica tus{' '}
-          <span className="text-acentColor font-bold">Reservas</span>
-          </p>
-          {reservationsList.map((reserva) => (
-            <DetalleReserva key={reserva.id} reserva={reserva} />
-          ))}
-        </>
-      ) : (
-        <>
-          <h2 className="font-black text-3xl text-center text-acentColor">No hay Reservas</h2>
-          <p className="text-xl mt-5 mb-10 text-center text-white">
-            Comienza agregando tu Reserva{' '}
-            <span className="text-acentColor font-bold">y la verás aquí</span>
-          </p>
-        </>
-      )}
-    </div>
-  );
-}
-
-
-
-
-//este codigo muestra las reservas por usuarios:
-
-// import React, { useEffect, useState } from "react";
-// import DetalleReserva from "./DetalleReserva";
-// import { useAuth } from "../context/AuthContext";
+// import React from "react";
 // import useStore from "../userStore";
+// import DetalleReserva from "./DetalleReserva";
+
 // export default function ListadoReservas() {
-//   const { user: authUser } = useAuth(); // Obtener el usuario autenticado desde el contexto
-//   const [reservations, setReservations] = useState([]);
-//   const deleteReservation = useStore((state) => state.deleteReservation);
-//   const getReservationById = useStore((state) => state.getReservationById);
-
-//   const handleDeleteReservation = (reservationId) => {
-//     deleteReservation(reservationId);
-//     setReservations(reservations.filter(reserva => reserva.id !== reservationId));
-//   };
-
-//   const handleEditReservation = (reservationId) => {
-//     getReservationById(reservationId); // Establecer la reserva activa
-//   };
-
-
-
-//   useEffect(() => {
-//     if (authUser && authUser.id) {
-//       fetch(`http://localhost:3000/reservations?userId=${authUser.id}`)
-//         .then((response) => response.json())
-//         .then((data) => setReservations(data))
-//         .catch((error) => console.error('Error fetching reservations:', error));
-//     }
-//   }, [authUser]);
+//   const reservationsList = useStore((state) => state.reservations);
 
 //   return (
 //     <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll scrollbar scrollbar-thumb-acentColor scrollbar-thumb-radius">
-//       {reservations.length ? (
+//       {reservationsList.length ? (
 //         <>
-//           <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reservas</h2>
+//           <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reserva</h2>
 //           <p className="text-lg mt-5 text-center mb-10 text-white">Verifica tus{' '}
 //           <span className="text-acentColor font-bold">Reservas</span>
 //           </p>
-//           {reservations.map((reserva) => (
-//             <DetalleReserva key={reserva.id} reserva={reserva} handleDeleteReservation={handleDeleteReservation} handleEditReservation={handleEditReservation} />
+//           {reservationsList.map((reserva) => (
+//             <DetalleReserva key={reserva.id} reserva={reserva} />
 //           ))}
 //         </>
 //       ) : (
@@ -90,3 +30,63 @@ export default function ListadoReservas() {
 //     </div>
 //   );
 // }
+
+
+
+
+//este codigo muestra las reservas por usuarios:
+
+import React, { useEffect, useState } from "react";
+import DetalleReserva from "./DetalleReserva";
+import { useAuth } from "../context/AuthContext";
+import useStore from "../userStore";
+export default function ListadoReservas() {
+  const { user: authUser } = useAuth(); // Obtener el usuario autenticado desde el contexto
+  const [reservations, setReservations] = useState([]);
+  const deleteReservation = useStore((state) => state.deleteReservation);
+  const getReservationById = useStore((state) => state.getReservationById);
+
+  const handleDeleteReservation = (reservationId) => {
+    deleteReservation(reservationId);
+    setReservations(reservations.filter(reserva => reserva.id !== reservationId));
+  };
+
+  const handleEditReservation = (reservationId) => {
+    getReservationById(reservationId); // Establecer la reserva activa
+  };
+
+
+
+  useEffect(() => {
+    if (authUser && authUser.id) {
+      fetch(`http://localhost:3000/reservations?userId=${authUser.id}`)
+        .then((response) => response.json())
+        .then((data) => setReservations(data))
+        .catch((error) => console.error('Error fetching reservations:', error));
+    }
+  }, [authUser]);
+
+  return (
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll scrollbar scrollbar-thumb-acentColor scrollbar-thumb-radius">
+      {reservations.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reservas</h2>
+          <p className="text-lg mt-5 text-center mb-10 text-white">Verifica tus{' '}
+          <span className="text-acentColor font-bold">Reservas</span>
+          </p>
+          {reservations.map((reserva) => (
+            <DetalleReserva key={reserva.id} reserva={reserva} handleDeleteReservation={handleDeleteReservation} handleEditReservation={handleEditReservation} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center text-acentColor">No hay Reservas</h2>
+          <p className="text-xl mt-5 mb-10 text-center text-white">
+            Comienza agregando tu Reserva{' '}
+            <span className="text-acentColor font-bold">y la verás aquí</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+}
