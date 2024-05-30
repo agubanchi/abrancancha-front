0042ef45ae7dbba315666f2e567ff685e97ab5a9
agustin banchi <agustinbanchi@MacBook-Air-de-agustin.local>
2024-05-30 12:18:50 -0300

    se soluciono el problema de mostrar la lista de reservas por usuario, ahora se permite eliminar o editar dicha reserva por el usuario

--- src/components/ListadoReservas.jsx
+++ src/components/ListadoReservas.jsx
@@ -1,20 +1,30 @@
-// ListadoUsuarios.js
-import React from "react";
-import useStore from "../userStore";
+import React, { useEffect } from "react";
 import DetalleReserva from "./DetalleReserva";
+import useStore from "../userStore";
+import { useAuth } from "../context/AuthContext";
 
 export default function ListadoReservas() {
-  const reservationsList = useStore((state) => state.reservations);
+  const { user: authUser } = useAuth();
+  const reservations = useStore((state) => state.reservations);
+
+  useEffect(() => {
+    if (authUser && authUser.id) {
+      fetch(`http://localhost:3000/reservations?userId=${authUser.id}`)
+        .then((response) => response.json())
+        .then((data) => useStore.setState({ reservations: data })) // Actualiza las reservas en el store
+        .catch((error) => console.error('Error fetching reservations:', error));
+    }
+  }, [authUser]);
 
   return (
     <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll scrollbar scrollbar-thumb-acentColor scrollbar-thumb-radius">
-      {reservationsList.length ? (
+      {reservations.length ? (
         <>
-          <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reserva</h2>
+          <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reservas</h2>
           <p className="text-lg mt-5 text-center mb-10 text-white">Verifica tus{' '}
           <span className="text-acentColor font-bold">Reservas</span>
           </p>
-          {reservationsList.map((reserva) => (
+          {reservations.map((reserva) => (
             <DetalleReserva key={reserva.id} reserva={reserva} />
           ))}
         </>
@@ -30,63 +40,3 @@ export default function ListadoReservas() {
     </div>
   );
 }
-
-
-
-
-//este codigo muestra las reservas por usuarios:
-
-// import React, { useEffect, useState } from "react";
-// import DetalleReserva from "./DetalleReserva";
-// import { useAuth } from "../context/AuthContext";
-// import useStore from "../userStore";
-// export default function ListadoReservas() {
-//   const { user: authUser } = useAuth(); // Obtener el usuario autenticado desde el contexto
-//   const [reservations, setReservations] = useState([]);
-//   const deleteReservation = useStore((state) => state.deleteReservation);
-//   const getReservationById = useStore((state) => state.getReservationById);
-
-//   const handleDeleteReservation = (reservationId) => {
-//     deleteReservation(reservationId);
-//     setReservations(reservations.filter(reserva => reserva.id !== reservationId));
-//   };
-
-//   const handleEditReservation = (reservationId) => {
-//     getReservationById(reservationId); // Establecer la reserva activa
-//   };
-
-
-
-//   useEffect(() => {
-//     if (authUser && authUser.id) {
-//       fetch(`http://localhost:3000/reservations?userId=${authUser.id}`)
-//         .then((response) => response.json())
-//         .then((data) => setReservations(data))
-//         .catch((error) => console.error('Error fetching reservations:', error));
-//     }
-//   }, [authUser]);
-
-//   return (
-//     <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll scrollbar scrollbar-thumb-acentColor scrollbar-thumb-radius">
-//       {reservations.length ? (
-//         <>
-//           <h2 className="font-black text-3xl text-center text-acentColor">Listado de Reservas</h2>
-//           <p className="text-lg mt-5 text-center mb-10 text-white">Verifica tus{' '}
-//           <span className="text-acentColor font-bold">Reservas</span>
-//           </p>
-//           {reservations.map((reserva) => (
-//             <DetalleReserva key={reserva.id} reserva={reserva} handleDeleteReservation={handleDeleteReservation} handleEditReservation={handleEditReservation} />
-//           ))}
-//         </>
-//       ) : (
-//         <>
-//           <h2 className="font-black text-3xl text-center text-acentColor">No hay Reservas</h2>
-//           <p className="text-xl mt-5 mb-10 text-center text-white">
-//             Comienza agregando tu Reserva{' '}
-//             <span className="text-acentColor font-bold">y la verás aquí</span>
-//           </p>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
