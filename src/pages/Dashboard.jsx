2b29fb4a7c1997d1c67f50fd959e5720d50d8a37
agustin banchi <agustinbanchi@MacBook-Air-de-agustin.local>
2024-06-13 20:08:45 -0300

    se actualizo dashboard para poder editar las reservas del usuario y que no afecte a su nombre, email y telefono

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -57,18 +57,8 @@ export default function Dashboard() {
           if (!response.ok) {
             throw new Error('Error al eliminar la reserva');
           }
-          setReservations((prevReservations) =>
-            prevReservations.filter((reserva) => reserva.id !== id)
-          );
-          Swal.fire({
-            title: 'Eliminado!',
-            text: 'La reserva ha sido eliminada.',
-            icon: 'success',
-            color: '#1d1d1d',
-            iconColor: '#1d1d1d',
-            confirmButtonColor: '#77da7e',
-            cancelButtonColor: '#1d1d1d',
-          });
+          setReservations(prevReservations => prevReservations.filter(reserva => reserva.id !== id));
+          Swal.fire('Eliminado!', 'La reserva ha sido eliminada.', 'success');
         } catch (error) {
           console.error('Error al eliminar la reserva:', error);
           Swal.fire(
