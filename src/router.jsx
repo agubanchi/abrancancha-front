9b447a01d435bd24d5e577dddb84d72e4ec64679
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-07-02 19:49:56 -0300

    Creacion e implementacion de listadoAdmins dentro del dashboard de admins

--- src/router.jsx
+++ src/router.jsx
@@ -10,6 +10,7 @@ import Home from "./pages/Home";
 import Logueo from "./Layouts/Logueo";
 import PrivateRoute from "./components/utils/PrivateRoute";
 import Layout from "./Layouts/Layout";
+import ListadoAdmins from "./pages/ListadoAdmins";
 
 const AppRouter = () => {
   return (
@@ -27,6 +28,7 @@ const AppRouter = () => {
     </Route>
     <Route element={<Layout/>}>
     <Route path='/dashboard' element={<Dashboard/>} roles={["admin"]}/>
+    <Route path='/ListadoAdmins' element={<ListadoAdmins />} roles={["admin"]}/>
     </Route>
     </Route>
     
