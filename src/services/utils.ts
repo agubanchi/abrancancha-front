7598c66e13e64087e8f4293de70859e022bf45f2
Mario Patronelli <mar.ioepatronelli@gmail.com>
2024-07-02 22:11:03 -0300

    Correccion de endpoints e implementacion de funciones para manejo de fechas

---
+++ src/services/utils.ts
@@ -0,0 +1,14 @@
+export function combinarFechaYHora(fechaString, horaString) {
+    const [dia, mes, año] = fechaString.split('/').map(Number);
+    const [hora, minuto, segundo] = horaString.split(':').map(Number);
+    // El mes en JavaScript se cuenta desde 0 (enero = 0, febrero = 1, ...)
+    return new Date(año, mes - 1, dia, hora, minuto, segundo);
+}
+
+function dateFormatToYMD(fechaString:string) {
+    let [dia, mes, año] = fechaString.split(/[-\/]/); // Permitir tanto '-' como '/' como separadores
+    // Asegurar que el mes y el día tengan dos dígitos (agregar ceros a la izquierda si es necesario)
+    mes = mes.padStart(2, '0');
+    dia = dia.padStart(2, '0');
+    return `${año}-${mes}-${dia}`;
+}
