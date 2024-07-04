fae7b270327a094e1e66b1ae28879399802813ee
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-07-04 19:35:27 -0300

    Traje la actualizacion de la rama DEV , gflata adherir funcionalidades al dashboard de admins

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -27,7 +27,11 @@ const HeaderDashboard = () => {
             <div className='w-3/4 items-center gap-6 text-center flex justify-center'>
             <NavLink to="/canchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Canchas</NavLink>
             <NavLink to="/reservations" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Reservas</NavLink>
+<<<<<<< HEAD
             <NavLink to="/admins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
+=======
+            <NavLink to="/ListadoAdmins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
+>>>>>>> 9b447a0 (Creacion e implementacion de listadoAdmins dentro del dashboard de admins)
             <NavLink to="/users" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Usuarios</NavLink>
             <NavLink to="/tiposCanchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Tipos de cancha</NavLink>
             <NavLink to="/estadosCanchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Estados de cancha</NavLink>
--- src/router.jsx
+++ src/router.jsx
@@ -1,54 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider, Role } from './context/AuthContext';
-import RegisterUser from './components/RegisterUser';
-import LoginUser from './components/LoginUser';
-import Reservations from './pages/Reservations';
-import Reservas from './pages/Reservas';
-import ProtectedRoute from './components/utils/ProtectedRoute'; // Importar ProtectedRoute
-import Home from './pages/Home';
-import Logueo from './Layouts/Logueo';
-import PrivateRoute from './components/utils/PrivateRoute';
-import Layout from './Layouts/Layout';
-import Users from './pages/Users';
-import DetailsTables from './pages/DetailsTables';
-import Admins from './pages/Admins';
-import Canchas from './pages/Canchas';
-import { Endpoint } from './services/fetchs';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import RegisterUser from "./components/RegisterUser";
+import LoginUser from "./components/LoginUser";
+import Reservations from "./pages/Reservations"
+import Reservas from "./pages/Reservas";
+import ProtectedRoute from "./components/utils/ProtectedRoute"; // Importar ProtectedRoute
+import Home from "./pages/Home";
+import Logueo from "./Layouts/Logueo";
+import PrivateRoute from "./components/utils/PrivateRoute";
+import Layout from "./Layouts/Layout";
+import ListadoAdmins from "./pages/ListadoAdmins";
 
 const AppRouter = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} index />
-          <Route element={<Logueo />}>
-            <Route path="/login" element={<LoginUser />} />
-            <Route path="/registrar" element={<RegisterUser />} />
-          </Route>
-          <Route element={<PrivateRoute roles={[Role.User, Role.Admin]} />}>
-            <Route element={<Logueo />}>
-              <Route path="/reservas" element={<Reservas />} roles={[Role.User]} />
-            </Route>
-            <Route element={<Layout />}>
-              <Route path="/canchas" element={<Canchas />} roles={[Role.Admin]} />
-              <Route
-                path="/reservations"
-                element={<Reservations />}
-                roles={[Role.Admin]}
-              />
-              {/* <Route path="/users" element={<Users />} roles={[Role.Admin]} />
-              <Route path="/admins" element={<Admins />} roles={[Role.Admin]} /> */}
-              <Route path="/users" element={<Users endPoint={Endpoint.users} />} />
-              <Route path="/admins" element={<Users endPoint={Endpoint.administrators} />} />
-              <Route path="/tiposCanchas" element={<DetailsTables endPoint={Endpoint.typesOfCourt}/>} />
-              <Route path="/estadosCanchas" element={<DetailsTables endPoint={Endpoint.statusOfCourt}/>} />
-              <Route path="/estadosReservas" element={<DetailsTables endPoint={Endpoint.statusOfReservation}/>} />
-              <Route path="/estadosUsuarios" element={<DetailsTables endPoint={Endpoint.statusOfUser}/>} />
-            </Route>
-          </Route>
-          {/* <Route path='*' element={<Missing/>} /> */}
-        </Routes>
+      <Routes>
+    <Route path='/' element={<Home/>} index/>
+    <Route element={<Logueo/>}>
+    <Route path='/login' element={<LoginUser />} />
+    <Route path='/registrar' element={<RegisterUser />} />
+    </Route>
+    <Route element={<PrivateRoute roles={["user", "admin"]}/>}>
+    <Route element={<Logueo/>}>
+    <Route path='/reservas' element={<Reservas/>} roles={["user"]}/>
+    </Route>
+    <Route element={<Layout/>}>
+    <Route path='/dashboard' element={<Dashboard/>} roles={["admin"]}/>
+    <Route path='/ListadoAdmins' element={<ListadoAdmins />} roles={["admin"]}/>
+    </Route>
+    </Route>
+    
+   </Routes>
       </BrowserRouter>
     </AuthProvider>
   );
