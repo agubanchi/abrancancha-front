314c914c39fa5e9a85b0df9683d5dbd55c431c92
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-07-04 19:35:27 -0300

    Traje la actualizacion de la rama DEV , gflata adherir funcionalidades al dashboard de admins

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -26,7 +26,11 @@ const HeaderDashboard = () => {
             <div className='w-3/4 items-center gap-6 text-center flex justify-center'>
             <NavLink to="/canchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Canchas</NavLink>
             <NavLink to="/reservations" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Reservas</NavLink>
+<<<<<<< HEAD
             <NavLink to="/admins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
+=======
+            <NavLink to="/ListadoAdmins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
+>>>>>>> 9b447a0 (Creacion e implementacion de listadoAdmins dentro del dashboard de admins)
             <NavLink to="/users" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Usuarios</NavLink>
             </div>
             <div className='gap-2 flex items-center'>
