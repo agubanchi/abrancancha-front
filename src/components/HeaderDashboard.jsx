accefeec19022167cc81d1271e85995021e65d9b
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-07-04 19:52:06 -0300

    Traje la actualizacion de la rama DEV , gflata adherir funcionalidades al dashboard de admins

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -27,11 +27,7 @@ const HeaderDashboard = () => {
             <div className='w-3/4 items-center gap-6 text-center flex justify-center'>
             <NavLink to="/canchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Canchas</NavLink>
             <NavLink to="/reservations" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Reservas</NavLink>
-<<<<<<< HEAD
-            <NavLink to="/admins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
-=======
             <NavLink to="/ListadoAdmins" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Administradores</NavLink>
->>>>>>> 9b447a0 (Creacion e implementacion de listadoAdmins dentro del dashboard de admins)
             <NavLink to="/users" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Usuarios</NavLink>
             <NavLink to="/tiposCanchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Tipos de cancha</NavLink>
             <NavLink to="/estadosCanchas" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Estados de cancha</NavLink>
@@ -42,8 +38,7 @@ const HeaderDashboard = () => {
             <NavLink to="/estadosUsuarios" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Horarios</NavLink>
             <NavLink to="/estadosUsuarios" className="hover:text-acentColor border px-2 py-2  border-acentColor rounded-md">Configuracion</NavLink>
             </div>
-            <div className='gap-2 flex items-center'>
-              {/* <button onClick={handleLogout} className="bg-acentColor font-Bebas text-2xl text-textColor py-3 px-12 rounded-full hover:bg-white hover:text-acentColor">Cerrar Sesión</button> */}
+            <div className='gap-2 flex items-center'>a
               {currentUser ? (
                 <ButtonCerrarSesion/>
               ) : (
--- src/router.jsx
+++ src/router.jsx
@@ -10,7 +10,8 @@ import Home from "./pages/Home";
 import Logueo from "./Layouts/Logueo";
 import PrivateRoute from "./components/utils/PrivateRoute";
 import Layout from "./Layouts/Layout";
-import ListadoAdmins from "./pages/ListadoAdmins";
+import Admins from "./pages/Admins";
+
 
 const AppRouter = () => {
   return (
@@ -28,7 +29,7 @@ const AppRouter = () => {
     </Route>
     <Route element={<Layout/>}>
     <Route path='/dashboard' element={<Dashboard/>} roles={["admin"]}/>
-    <Route path='/ListadoAdmins' element={<ListadoAdmins />} roles={["admin"]}/>
+    <Route path='/Admins' element={<Admins />} roles={["admin"]}/>
     </Route>
     </Route>
     
