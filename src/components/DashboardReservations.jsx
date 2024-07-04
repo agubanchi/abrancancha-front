0c9f5961199411c1e6edf43e7706fa7ac316a67f
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2024-07-04 20:57:31 -0300

    Administradores en proceso

--- src/components/DashboardReservations.jsx
+++ src/components/DashboardReservations.jsx
@@ -11,13 +11,13 @@ const DashboardReservations = ({ reserva, removeReservation, handleEdit }) => {
   return (
     <>
       <tr className='text-center text-white flex justify-between gap-2 w-full px-4 py-2'>
-        <td className="content-start w-40">{reserva.user ? reserva.user.fullname : "Usuario no encontrado"}</td>
-        <td className="content-start w-40">{reserva.user ? reserva.user.email : "Usuario no encontrado"}</td>
-        <td className="content-start w-40">{reserva.user ? reserva.user.phone : "Usuario no encontrado"}</td>
-        <td className="content-start w-40">{reserva.court ? reserva.court.name : "Cancha no encontrada"}</td>
-        <td className="content-start w-40">{reserva.court ? reserva.court.type : "Tipo no encontrado"}</td>
-        <td className="content-start w-40">{new Date(reserva.timedate).toLocaleString()}</td>
-        <td className="content-start w-40">{reserva.status ? reserva.status.name : 'Pendiente'}</td>
+        <td className="content-start w-40">{user ? user.fullname : "Usuario no encontrado"}</td>
+        <td className="content-start w-40">{user ? user.email : "Usuario no encontrado"}</td>
+        <td className="content-start w-40">{user ? user.phone : "Usuario no encontrado"}</td>
+        <td className="content-start w-40">{reserva.cancha}</td>
+        <td className="content-start w-40">{reserva.tipo}</td>
+        <td className="content-start w-40">{reserva.date}</td>
+        <td className="content-start w-40">{reserva.state}</td>
         <td className='flex justify-around py-2 gap-2 items-center w-40'>
           <FaEdit className="cursor-pointer" onClick={handleOpenModal} />
           <MdDelete className="cursor-pointer" onClick={() => removeReservation(reserva.id)} />
